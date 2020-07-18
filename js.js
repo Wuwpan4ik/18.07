f40d0cd808184f86f14ccab1d2859b520f08ad47
Dima <61015330+Wuwpan4ik@users.noreply.github.com>
2020-07-18 14:55:55 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	// $('.header-burger').click(function(){
+	// 	$('.header-burger-list').toggleClass('active');
+	// 	alert("Привет");
+	// });
+	$('.header-burger').click(function(){
+		$('.header-burger-list').slideToggle().toggleClass('active');
+		$('.header-burger').toggleClass('active');
+		$('body').toggleClass('active');
+	});
+
+
+	$('.header-input').click(function(){
+		$('.header-form').slideToggle().toggleClass('active');
+	});
+
+
+	$('.filter-list').click(function(){
+		$(this).next('.filter-list-wrapper').slideToggle();
+		$(this).children('.filter-arrow').toggleClass('fa-angle-down').toggleClass('fa-angle-up');
+	});
+
+
+	$('.main-filter').click(function(){
+		$('.filter-second-list').slideToggle().toggleClass('active');
+	});
+});
+ymaps.ready(init);
+function init(){
+	var map = new ymaps.Map('map',{
+		center: [59.94, 30.32],
+		zoom: 13,
+		controls: ['zoomControl']
+	});
+
+
+	var placemark = new ymaps.Placemark( [59.94, 30.32],{
+		hintContent: '<div class="map-hint"> Инвест торги</div><div class="map-hint-text">Закрыто на вход</div>'
+	},
+	{
+		iconLayout: 'default#image',
+		iconImageHref: '../img/AIrplanes.png',
+		iconImageSize: [20, 25]
+	});
+	map.geoObjects.add(placemark);
+}
